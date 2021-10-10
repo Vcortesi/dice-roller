9f2eca20c589a669f3e9fdfac44ad0eb3b3c8bb2
Vincent Cortesi <vincentcortesi@gmail.com>
2021-10-10 15:04:14 -0500

    Initial Dice Roller

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Function to roll the dice
+function roll() {
+        var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+        document.querySelector(".img1").setAttribute("src",
+            "dice" + randomNumber1 + ".png");
+        document.querySelector(".img2").setAttribute("src",
+            "dice" + randomNumber2 + ".png");
+}
